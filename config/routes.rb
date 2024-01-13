1fa5faed13fcbd3515e8d3b14514f9bf7cb5ec9c
ayu10293 <snof11513@gmail.com>
2024-01-13 09:35:38 +0000

    min,max

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'calculations/show'
   post 'calculations' => 'calculations#create'
   get '/top' => 'homes#top'
-  root to: 'homes#top'
+  root to: 'rare_calculation#show'
   get 'rare_calculation/show'
   post 'rare_calculation/result'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
